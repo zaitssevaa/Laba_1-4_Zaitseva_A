239ee8d58c1e64ba648371e9e726eb58ea023433
zaitssevaa <Anna-Andreevna.2001@yandex.ru>
2022-03-22 16:28:59 +0300

    Add ks

--- Zaitseva/Zaitseva/Zaitseva.cpp
+++ Zaitseva/Zaitseva/Zaitseva.cpp
@@ -14,6 +14,13 @@ struct pipe {
     bool repair;
 };
 
+struct KS {
+    unsigned int id;
+    string Name;
+    unsigned int Count, CountInWork;
+    double Efficiency;
+};
+
 bool CheckDouble(string input)
 {
     int length, i, point_count = 0;
@@ -61,6 +68,37 @@ bool CheckInt(string input)
     return true;
 }
 
+bool CheckIDPipe(vector<pipe>& pipes, int newID)
+{
+    int i;
+    if (pipes.size() > 0)
+    {
+        for (i = 0; i < pipes.size(); ++i)
+        {
+            if (newID == pipes[i].id)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool CheckIDKompress(vector<KS>& kompress, int newID)
+{
+    int i;
+    if (kompress.size() > 0)
+    {
+        for (i = 0; i < kompress.size(); ++i)
+        {
+            if (newID == kompress[i].id)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 void AddPipeline(vector<pipe>& pipes)
 {
@@ -72,8 +110,14 @@ void AddPipeline(vector<pipe>& pipes)
     {
         getline(cin, input);
         if (CheckInt(input)) {
-            pipes[pipes.size() - 1].id = stoi(input);
-            break;
+            if (!CheckIDPipe(pipes, stoi(input))) {
+                pipes[pipes.size() - 1].id = stoi(input);
+                break;
+            }
+            else
+            {
+                cout << "Такой ID уже существует, попробуйте еще раз: ";
+            }
         }
         else {
             cout << "Введен некорректный символ, попробуйте еще раз: ";
@@ -117,7 +161,77 @@ void AddPipeline(vector<pipe>& pipes)
     }
 }
 
-void ShowPipeline(vector<pipe>& pipes)
+void AddKC(vector<KS>& kompress)
+{
+    system("cls");
+    string input;
+    cout << "Введите характеристики КС: \nID: ";
+    kompress.resize(kompress.size() + 1);
+    while (true)
+    {
+        getline(cin, input);
+        if (CheckInt(input)) {
+            if (!CheckIDKompress(kompress, stoi(input))) {
+                kompress[kompress.size() - 1].id = stoi(input);
+                break;
+            }
+            else
+            {
+                cout << "Такой ID уже существует, попробуйте еще раз: ";
+            }
+        }
+        else {
+            cout << "Введен некорректный символ, попробуйте еще раз: ";
+        }
+    }
+    cout << "Название: ";
+    getline(cin, input);
+    kompress[kompress.size() - 1].Name = input;
+    cout << "Количество цехов: ";
+    while (true)
+    {
+        getline(cin, input);
+        if (CheckInt(input)) {
+            kompress[kompress.size() - 1].Count = stoi(input);
+            break;
+        }
+        else {
+            cout << "Введен некорректный символ, попробуйте еще раз: ";
+        }
+    }
+    cout << "Количество цехов в работе: ";
+    while (true)
+    {
+        getline(cin, input);
+        if (CheckInt(input)) {
+            if (stoi(input) <= kompress[kompress.size() - 1].Count) {
+                kompress[kompress.size() - 1].CountInWork = stoi(input);
+                break;
+            }
+            else
+            {
+                cout << "Количество цехов в работе не может быть больше общего кол-ва, поробуйте еще раз: ";
+            }
+        }
+        else {
+            cout << "Введен некорректный символ, попробуйте еще раз: ";
+        }
+    }
+    cout << "Эффективность: ";
+    while (true)
+    {
+        getline(cin, input);
+        if (CheckDouble(input)) {
+            kompress[kompress.size() - 1].Efficiency = stod(input);
+            break;
+        }
+        else {
+            cout << "Введен некорректный символ, попробуйте еще раз: ";
+        }
+    }
+}
+
+void ShowPipeline(vector<pipe>& pipes, vector<KS>& kompress)
 {
     system("cls");
     int i;
@@ -139,15 +253,36 @@ void ShowPipeline(vector<pipe>& pipes)
     else {
         cout << "Массив пустой";
     }
-
+    cout << "Компрессорные станции" << endl <<
+        setw(10) << "ID" <<
+        setw(20) << "Название" <<
+        setw(20) << "Кол-во цехов" <<
+        setw(20) << "Цехов в работе" <<
+        setw(20) << "Эффективность" << endl;
+    if (kompress.size() > 0)
+    {
+        for (i = 0; i < kompress.size(); ++i)
+        {
+            cout << setw(10) << kompress[i].id <<
+                setw(20) << kompress[i].Name <<
+                setw(20) << kompress[i].Count <<
+                setw(20) << kompress[i].CountInWork <<
+                setw(20) << kompress[i].Efficiency << endl;
+        }
+    }
+    else {
+        cout << "Массив пустой";
+    }
 }
 
 int main()
 {
     setlocale(LC_CTYPE, "Russian");
     vector<pipe> pipes;
+    vector<KS> kompress;
     char inputmenu;
     pipes.resize(0);
+    kompress.resize(0);
     while (true)
     {
         system("cls");
@@ -173,7 +308,7 @@ int main()
             break;
         case '2':
             system("cls");
-            cout << "2\n";
+            AddKC(kompress);
             while (true) {
                 if (_getch() == '0') {
                     break;
@@ -182,7 +317,7 @@ int main()
             break;
         case '3':
             system("cls");
-            ShowPipeline(pipes);
+            ShowPipeline(pipes, kompress);
             while (true) {
                 if (_getch() == '0') {
                     break;
